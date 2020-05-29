08e3376e502a90538cb1628488f913141921697f
Joonmo Yang <dev@remagpie.com>
2020-05-29 12:50:19 +0900

    Use typesafe-actions for managing redux.

--- src/store/buffer.ts
+++ src/store/buffer.ts
@@ -1,9 +1,27 @@
-import {createReducer} from "./util";
+import produce from "immer";
+import {createAction, createReducer} from "typesafe-actions";
+import type {ActionType} from "typesafe-actions";
 
-export type State = {};
+import type {TextBuffer} from "../types/buffer";
+
+export type State = {
+	[key: string]: TextBuffer;
+};
 
 const initialState: State = {};
 
-export type Action = never;
-export const reducer = createReducer(initialState, (_state, _action: Action) => {
+export const setBuffer = createAction("buffer/set")<{
+	id: string;
+}>();
+
+export type Action =
+	| ActionType<typeof setBuffer>;
+
+export const reducer = createReducer<State, Action>(initialState, {
+	"buffer/set": (state, action) => produce(state, (s) => {
+		const {id} = action.payload;
+		s[id] = {
+			id,
+		};
+	}),
 });
