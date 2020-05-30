4602a25b1177d422f2f2367690e51b8f2da05fdc
Joonmo Yang <dev@remagpie.com>
2020-05-31 05:33:02 +0900

    Add the command store

---
+++ src/store/command.ts
@@ -0,0 +1,28 @@
+import produce from "immer";
+import {createAction, createReducer} from "typesafe-actions";
+import type {ActionType} from "typesafe-actions";
+
+import Command from "../types/command";
+import {State as RootState} from "./index";
+
+export type State = {
+	[id: string]: Command;
+};
+
+const initialState: State = {};
+
+export const selectState = (state: RootState): State => state.command;
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const selectCommand = (id: string) => (state: RootState) => selectState(state)[id];
+
+export const putCommand = createAction("command/put")<Command>();
+
+export type Action =
+	| ActionType<typeof putCommand>;
+
+export const reducer = createReducer<State, Action>(initialState, {
+	"command/put": (state, action) => produce(state, (s) => {
+		const command = action.payload;
+		s[command.id] = command;
+	}),
+});
