bd337704f91732899b14ffc3729d7a1eb6e5ab2c
Joonmo Yang <dev@remagpie.com>
2020-06-22 16:01:01 +0900

    Register the plugin dynamically to the store

--- src/plugins/shortcut/index.ts
+++ src/plugins/shortcut/index.ts
@@ -1,18 +1,59 @@
-import type {Dispatch} from "../../store";
-import {enterKey} from "../../store/shortcut";
+import produce from "immer";
+import {createAction, createReducer} from "typesafe-actions";
+import type {ActionType} from "typesafe-actions";
 
-function createOnKeyDown(dispatch: Dispatch) {
-	return (event: KeyboardEvent) => {
-		dispatch(enterKey(event.code));
-		event.stopImmediatePropagation();
+import {deleteReducer, putReducer} from "../../store";
+import type {Dispatch, State as RootState, ThunkAction} from "../../store";
+import {runCommand} from "../../store/command";
+import Command from "../../types/command";
+
+export type State = {
+	[key: string]: Command["id"];
+};
+
+const initialState: State = {};
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-return
+export const selectState = (state: RootState): State => state.shortcut;
+export const selectBinding = (key: string) => (state: RootState) => selectState(state)[key];
+
+export const putBinding = createAction("shortcut/binding/put")<{
+	key: string;
+	command: Command["id"];
+}>();
+
+export function enterKey(code: string): ThunkAction<void> {
+	return (dispatch, getState) => {
+		const state = getState();
+		const command = selectBinding(code)(state);
+
+		if (command != null) {
+			dispatch(runCommand(command));
+		}
 	};
 }
 
+type Action =
+	| ActionType<typeof putBinding>;
+
+const reducer = createReducer<State, Action>(initialState, {
+	"shortcut/binding/put": (state, action) => produce(state, (s) => {
+		const {key, command} = action.payload;
+		s[key] = command;
+	}),
+});
+
 export default function effect(dispatch: Dispatch) {
-	const onKeyDown = createOnKeyDown(dispatch);
+	dispatch(putReducer("shortcut", reducer));
+
+	const onKeyDown = (event: KeyboardEvent) => {
+		dispatch(enterKey(event.code));
+		event.stopImmediatePropagation();
+	};
 	document.addEventListener("keydown", onKeyDown);
 
 	return function () {
 		document.removeEventListener("keydown", onKeyDown);
+		deleteReducer("shortcut");
 	};
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,36 +1,59 @@
+import produce from "immer";
 import {useDispatch as rawUseDispatch} from "react-redux";
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import {applyMiddleware, createStore} from "redux";
+import type {AnyAction, Reducer} from "redux";
 import thunk from "redux-thunk";
 import type {ThunkAction as RawThunkAction, ThunkDispatch} from "redux-thunk";
 
 import * as buffer from "./buffer";
 import * as command from "./command";
 import * as pane from "./pane";
-import * as shortcut from "./shortcut";
 
 export type State = {
 	buffer: buffer.State;
 	command: command.State;
 	pane: pane.State;
-	shortcut: shortcut.State;
+	[plugin: string]: any;
 };
 
-type Action =
-	| buffer.Action
-	| command.Action
-	| pane.Action
-	| shortcut.Action;
+type BuiltinAction = buffer.Action | command.Action | pane.Action;
+type Action = BuiltinAction | AnyAction;
 export type ThunkAction<R> = RawThunkAction<R, State, undefined, Action>;
 export type AsyncThunkAction<R> = ThunkAction<Promise<R>>;
 
 export type Dispatch = ThunkDispatch<State, undefined, Action>;
 
-const reducer = combineReducers({
+// TODO: Use a better type for the reducer
+const reducerMap: Record<string, Reducer<any, any>> = {
 	buffer: buffer.reducer,
 	command: command.reducer,
 	pane: pane.reducer,
-	shortcut: shortcut.reducer,
-});
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+function reducer(state: State = {} as any, action: Action) {
+	return produce(state, (s) => {
+		for (const [key, childReducer] of Object.entries(reducerMap)) {
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+			s[key] = childReducer(s[key], action as any);
+		}
+	});
+}
+
+// TODO: Use a better type for the reducer
+export function putReducer(name: string, childReducer: Reducer<any, any>): ThunkAction<void> {
+	return (dispatch) => {
+		reducerMap[name] = childReducer;
+		dispatch({type: name + "/init"});
+	};
+}
+
+export function deleteReducer(name: string): ThunkAction<void> {
+	return (dispatch) => {
+		delete reducerMap[name];
+		dispatch({type: name + "/destroy"});
+	};
+}
 
 export default createStore(reducer, applyMiddleware(thunk));
 
