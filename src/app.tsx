c9a69f25a68aa1c61d60c0712632809245131d97
Joonmo Yang <dev@remagpie.com>
2021-06-30 00:54:08 +0900

    Use react

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,3 @@
-import {h} from "preact";
-
 const App = () => <span>Hello, World!</span>;
 App.displayName = "App";
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import {h, render} from "preact";
+import {render} from "react-dom";
 
 import App from "./app";
 
 const Root = () => <App />;
 Root.displayName = "Root";
 
-render(<Root />, document.getElementById("root")!);
+render(<Root />, document.getElementById("root"));
